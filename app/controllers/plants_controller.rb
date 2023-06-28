dcff343deca342b97bc9de6039800f4349587504
ned <nedschultz@outlook.com>
2023-06-28 18:37:33 +1000

    commit tree 62fec15e5cf094c196ab40c739d26069a9f0ed7b

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,21 +1,53 @@
 class PlantsController < ApplicationController
-
-  # GET /plants
+  
   def index
     plants = Plant.all
     render json: plants
   end
 
-  # GET /plants/:id
+  
   def show
     plant = Plant.find_by(id: params[:id])
-    render json: plant
+    if plant
+      render json: plant
+    else
+      render json: { error: 'Plant not found' }, status: :not_found
+    end
   end
 
-  # POST /plants
+  
   def create
     plant = Plant.create(plant_params)
-    render json: plant, status: :created
+    if plant.valid?
+      render json: plant, status: :created
+    else
+      render json: { errors: plant.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  
+  def update
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      if plant.update(plant_params)
+        render json: plant
+      else
+        render json: { errors: plant.errors.full_messages }, status: :unprocessable_entity
+      end
+    else
+      render json: { error: 'Plant not found' }, status: :not_found
+    end
+  end
+
+  
+  def destroy
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      plant.destroy
+      head :no_content
+    else
+      render json: { error: 'Plant not found' }, status: :not_found
+    end
   end
 
   private
